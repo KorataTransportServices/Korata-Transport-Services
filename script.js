f203446e33915a7f6295c4796a172d99b9fc278d
Kgopotso99 <103973354+Kgopotso99@users.noreply.github.com>
2024-10-07 21:17:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-document.getElementById("contact-form").addEventListener("submit", function(event) {
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
-
-    if (name === "" || email === "" || message === "") {
-        alert("Please fill in all fields before submitting.");
-        event.preventDefault(); // Prevent form submission
-    } else {
-        alert("Form submitted successfully!");
-    }
+document.getElementById("contact-form").addEventListener("submit", function (event) {
+    alert("Thank you for contacting K & T Transport Services. We will get back to you shortly!");
 });
