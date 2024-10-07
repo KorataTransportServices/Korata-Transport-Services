dda43e649d3857513c1ee3ced22ab508b60e5814
Kgopotso99 <103973354+Kgopotso99@users.noreply.github.com>
2024-10-07 21:17:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById("contact-form").addEventListener("submit", function (event) {
-    alert("Thank you for contacting K & T Transport Services. We will get back to you shortly!");
+    alert("Thank you for contacting Korata Transport Services. We will get back to you shortly!");
 });
